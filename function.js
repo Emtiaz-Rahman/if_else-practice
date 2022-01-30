ad3ccdf4340cafae8fa3a356937ca6d6e5beb246
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-31 02:36:18 +0600

    cost

--- function.js
+++ function.js
@@ -4,3 +4,42 @@ function multiplicationTable(number) {
 }
 var finalResult = multiplicationTable(10);
 console.log(finalResult);
+
+function kgToman(kg) {
+    var result = kg / 40;
+    return result;
+}
+var weight = kgToman(1000);
+console.log(weight + " mon");
+
+function mileTokm(mile) {
+    var result = mile * 1.60934;
+    return result;
+}
+var lenth = mileTokm(4);
+console.log(lenth + 'km');
+
+function totalQuantity(book, pen, note) {
+    var total = book + pen + note;
+    return total;
+
+}
+var book = 5;
+var pen = 20;
+var note = 90;
+var quantity = totalQuantity(book, pen, note);
+console.log('Total Quantity=', quantity + 'pcs')
+
+
+function totalCost(book, pen, note) {
+    var bookPrice = 10;
+    var penPrice = 5;
+    var notePrice = 10;
+    var total = (bookPrice * book) + (penPrice * pen) + (notePrice * note);
+    return total;
+}
+var book = 10;
+var pen = 300;
+var note = 60;
+var totalPrice = totalCost(book, pen, note);
+console.log('Total Cost=', totalPrice + 'taka');
