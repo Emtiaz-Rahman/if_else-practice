55f291a2dbe2d4c76def31dab856cdaafbfb592d
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-31 00:57:19 +0600

    first commit

---
+++ if_else.js
@@ -0,0 +1,21 @@
+var result = 110;
+
+if (result >= 90 && result <= 100) {
+    console.log('Grade:A+');
+}
+else if (result >= 80 && result < 90) {
+    console.log('Grade:A');
+}
+else if (result >= 70 && result < 80) {
+    console.log('Grade:B');
+}
+else if (result >= 60 && result < 70) {
+    console.log('Grade:C');
+}
+else if (result >= 50 && result < 60) {
+    console.log('Grade:D');
+}
+else {
+    console.log('Grade:F');
+}
+
