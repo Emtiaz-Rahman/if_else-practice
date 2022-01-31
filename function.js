bd51096514bc59bc045428b72b71403be8b0f3bd
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-31 11:10:38 +0600

    uppercase

--- function.js
+++ function.js
@@ -1,56 +1,63 @@
-function multiplicationTable(number) {
-    var result = 13 * number;
-    return result;
+// function multiplicationTable(number) {
+//     var result = 13 * number;
+//     return result;
+// }
+// var finalResult = multiplicationTable(10);
+// console.log(finalResult);
+
+// function kgToman(kg) {
+//     var result = kg / 40;
+//     return result;
+// }
+// var weight = kgToman(1000);
+// console.log(weight + " mon");
+
+// function mileTokm(mile) {
+//     var result = mile * 1.60934;
+//     return result;
+// }
+// var lenth = mileTokm(4);
+// console.log(lenth + 'km');
+
+// function totalQuantity(book, pen, note) {
+//     var total = book + pen + note;
+//     return total;
+
+// }
+// var book = 5;
+// var pen = 20;
+// var note = 90;
+// var quantity = totalQuantity(book, pen, note);
+// console.log('Total Quantity=', quantity + 'pcs')
+
+
+// function totalCost(book, pen, note) {
+//     var bookPrice = 10;
+//     var penPrice = 5;
+//     var notePrice = 10;
+//     var total = (bookPrice * book) + (penPrice * pen) + (notePrice * note);
+//     return total;
+// }
+// var book = 10;
+// var pen = 300;
+// var note = 60;
+// var totalPrice = totalCost(book, pen, note);
+// console.log('Total Cost=', totalPrice + 'taka');
+
+
+// function inputNumber(number) {
+//     var number = number ** 2;
+//     return number;
+// }
+// var value = 6;
+// var result = inputNumber(value);
+// console.log('Output=', result);
+
+
+function upperCase(name) {
+    var output = name.toLowerCase();
+    return output;
 }
-var finalResult = multiplicationTable(10);
-console.log(finalResult);
-
-function kgToman(kg) {
-    var result = kg / 40;
-    return result;
-}
-var weight = kgToman(1000);
-console.log(weight + " mon");
-
-function mileTokm(mile) {
-    var result = mile * 1.60934;
-    return result;
-}
-var lenth = mileTokm(4);
-console.log(lenth + 'km');
-
-function totalQuantity(book, pen, note) {
-    var total = book + pen + note;
-    return total;
-
-}
-var book = 5;
-var pen = 20;
-var note = 90;
-var quantity = totalQuantity(book, pen, note);
-console.log('Total Quantity=', quantity + 'pcs')
-
-
-function totalCost(book, pen, note) {
-    var bookPrice = 10;
-    var penPrice = 5;
-    var notePrice = 10;
-    var total = (bookPrice * book) + (penPrice * pen) + (notePrice * note);
-    return total;
-}
-var book = 10;
-var pen = 300;
-var note = 60;
-var totalPrice = totalCost(book, pen, note);
-console.log('Total Cost=', totalPrice + 'taka');
-
-
-function inputNumber(number) {
-    var number = number ** 2;
-    return number;
-}
-var value = 6;
-var result = inputNumber(value);
-console.log('Output=', result);
-
-
+var inputName = 'Emtiaz Rahman'
+var finalOutput = upperCase(inputName);
+console.log('Lower Case=', finalOutput);
